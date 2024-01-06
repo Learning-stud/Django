eec616bacb1eb6f5e6a2c5173cf9d693e974bcb4
Learning-stud <jayahalpara123456@gmail.com>
2024-01-06 19:12:10 +0530

    .

--- Second/project_name/app_name/models.py
+++ Second/project_name/app_name/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class usere(models.Model):
+ email = models.EmailField(unique=True, max_length=50)
+ password =models.CharField(max_length=20)
+ role = models.CharField( max_length=20)  # chairman/member 
