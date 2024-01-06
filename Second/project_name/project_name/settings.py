80dedde4fe9aa89d3a64e2b155f452ba1b0fc48a
Learning-stud <jayahalpara123456@gmail.com>
2024-01-06 19:13:34 +0530

    ...

--- Second/project_name/project_name/settings.py
+++ Second/project_name/project_name/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_name'
+
 ]
 
 MIDDLEWARE = [
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- Second/project_name/project_name/urls.py
+++ Second/project_name/project_name/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_name import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.loginHtml, name='login')
 ]
