1b388fcfef3ecab83235b237879738ff1ede9449
Learning-stud <jayahalpara123456@gmail.com>
2024-01-08 16:59:05 +0530

    ..

--- Second/project_name/app_name/models.py
+++ Second/project_name/app_name/models.py
@@ -3,7 +3,22 @@ from django.db import models
 # Create your models here.
 
 
-class usere(models.Model):
- email = models.EmailField(unique=True, max_length=50)
+class user(models.Model):
+ email = models.EmailField(unique=True,null=False, max_length=50)
  password =models.CharField(max_length=20)
- role = models.CharField( max_length=20)  # chairman/member 
+ role = models.CharField( max_length=20)  # chairman/member
+ isActive=models.BooleanField(default= False)
+ created_at= models.DateTimeField(auto_now= True)
+ def __str__(self):
+     return self.email+self.password
+
+class Chairman(models.Model):
+ userid= models.ForeignKey(user,on_delete=models.CASCADE)
+ firstname = models.CharField(max_length = 20)
+ lasttname = models.CharField(max_length = 20)
+ contact = models.IntegerField(max_length=10)
+ blockno= models.CharField(max_length=4)
+ houseno=models.CharField(max_length= 4)
+
+ def __str__(self):
+     return self.firstname
