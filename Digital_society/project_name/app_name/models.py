1dc3e99535b2ff81549c07d0e6f7e74bd6b63683
Learning-stud <jayahalpara123456@gmail.com>
2024-01-19 02:23:13 +0530

    ...,.,

--- Digital_society/project_name/app_name/models.py
+++ Digital_society/project_name/app_name/models.py
@@ -4,19 +4,19 @@ class User(models.Model):
     email = models.EmailField(unique=True, max_length=50)
     password = models.CharField(max_length=20)
     role = models.CharField(max_length=20)
-    is_active = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now=True)
+    isActive = models.BooleanField(default=False)
+    createdat = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.email} | {self.password}"
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
     contact = models.CharField(max_length=11)
-    block_no = models.CharField(max_length=4)
-    house_no = models.CharField(max_length=4)
+    blockno = models.CharField(max_length=4)
+    houseno = models.CharField(max_length=4)
 
     def __str__(self):
         return f"{self.first_name} | {self.block_no}"
@@ -26,9 +26,9 @@ class Chairman(Profile):
 
 class Member(Profile):
     occupation = models.CharField(max_length=20)
-    job_address = models.TextField()
-    blood_group = models.CharField(max_length=3)
-    vehicle_details = models.CharField(max_length=20)
+    jobaddress = models.TextField()
+    bloodgroup = models.CharField(max_length=3)
+    vehicledetails = models.CharField(max_length=20)
     pic = models.FileField(upload_to="media/upload", default="default.png")
 
     def __str__(self):
