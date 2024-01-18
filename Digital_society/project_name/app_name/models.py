211fa11b8f7ad3c8a0b259fb56bc7b8ea4bd3386
Learning-stud <jayahalpara123456@gmail.com>
2024-01-19 02:16:54 +0530

    DJANGO

--- Digital_society/project_name/app_name/models.py
+++ Digital_society/project_name/app_name/models.py
@@ -1,38 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True, max_length=50)
+    password = models.CharField(max_length=20)
+    role = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.email} | {self.password}"
 
-class user(models.Model):
- email = models.EmailField(unique=True,null=False, max_length=50)
- password =models.CharField(max_length=20)
- role = models.CharField( max_length=20)  # chairman/member
- isActive=models.BooleanField(default= False)
- created_at= models.DateTimeField(auto_now= True)
- def __str__(self):
-     return self.email+self.password
-
-class Chairman(models.Model):
- userid= models.ForeignKey(user,on_delete=models.CASCADE)
- firstname = models.CharField(max_length = 20)
- lasttname = models.CharField(max_length = 20)
- contact = models.IntegerField(max_length=10)
- blockno= models.CharField(max_length=4)
- houseno=models.CharField(max_length= 4)
-
- def __str__(self):
-        return self.firstname + " | "+self.blockno
-
-
-class Member(models.Model):
-    userid = models.ForeignKey(User,on_delete=models.CASCADE)
-    firstname = models.CharField(max_length = 20)
-    lastname = models.CharField(max_length=20)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
     contact = models.CharField(max_length=11)
-    blockno = models.CharField(max_length=3)
-    houseno = models.CharField(max_length=4)
+    block_no = models.CharField(max_length=4)
+    house_no = models.CharField(max_length=4)
+
+    def __str__(self):
+        return f"{self.first_name} | {self.block_no}"
+
+class Chairman(Profile):
+    pass
+
+class Member(Profile):
     occupation = models.CharField(max_length=20)
     job_address = models.TextField()
-    bloodgroup = models.CharField(max_length=3)
-    vehical_details = models.CharField(max_length=20)
-    pic = models.FileField(upload_to="media/upload",default="default.png")
+    blood_group = models.CharField(max_length=3)
+    vehicle_details = models.CharField(max_length=20)
+    pic = models.FileField(upload_to="media/upload", default="default.png")
+
+    def __str__(self):
+        return f"{self.first_name} | {self.block_no}"
